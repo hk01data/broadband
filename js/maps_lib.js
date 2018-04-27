326d8af7e524f7a5b5edff6ce139fe49d024bf1a
hk01data <bigdata@hk01.com>
2018-04-27 15:12:38 +0800

    Add files via upload

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -33,7 +33,7 @@ var MapsLib = {
 
   //*MODIFY Fusion Tables Requirement* API key. found at https://code.google.com/apis/console/
   //*Important* this key is for demonstration purposes. please register your own.
-  googleApiKey:       "AIzaSyBT6MZqTvRJt4A3BQGqofmHUOWLFApeBqo",
+  googleApiKey:       "AIzaSyD9G9s6XUSnnExim6pn0Y9vr2nAIvsfZPE",
 
   //MODIFY name of the location column in your Fusion Table.
   //NOTE: if your location column name has spaces in it, surround it with single quotes
